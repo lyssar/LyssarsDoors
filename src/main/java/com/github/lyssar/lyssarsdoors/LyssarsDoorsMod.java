282cff14851ee49c0b192208b3d306c22dde8545
Sebastian H <sebastian.hens@gmail.com>
2022-06-20 11:46:46 +0200

    chore(): changed some package naming, nothing breaking

--- src/main/java/com/github/lyssar/lyssarsdoors/LyssarsDoorsMod.java
+++ src/main/java/com/github/lyssar/lyssarsdoors/LyssarsDoorsMod.java
@@ -1,4 +1,4 @@
-package com.github.lyssar.lyssarsdoors.lyssarsdoors;
+package com.github.lyssar.lyssarsdoors;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,13 +18,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("lyssarsdoors")
-public class LyssarsDoors {
+@Mod(LyssarsDoorsMod.MOD_ID)
+public class LyssarsDoorsMod {
 
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "lyssarsdoors";
 
-    public LyssarsDoors() {
+    public LyssarsDoorsMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
